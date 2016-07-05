9db24edc40d7c880319eb55d9031f77534456b40
zainnadeem <zn.nadeem@gmail.com>
2016-07-05 13:56:37 -0400

    swift variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -10,3 +10,6 @@ let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
 ultimateFavoriteCharacter = "Daenerys Targaryen"
+
+
+//When should you use a variable, and when should you use a constant? A variable should be used when a value might change during the course of a single run of a program. For example, when you start running a program, your favorite character may be Jon Snow, but perhaps by the end of the program's run, you may want to switch your allegiance to another character (like when you changed the value of favoriteCharacter to "Tyrion Lannister"). A constant, on the other hand, is a way to bind a name to a value that won't change. This lets you refer to that value throughout your program where you are certain without a doubt that the value will never change.
